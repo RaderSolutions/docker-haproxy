c3a6e5e91d33adbc040478e702774b432566973c
Les Begnaud <lbegnaud@radersolutions.com>
2018-10-19 08:47:19 -0500

    final changes, but abandoned

--- Dockerfile
+++ Dockerfile
@@ -15,4 +15,4 @@ COPY docker-entrypoint.sh /
 RUN ["chmod", "a+x", "/docker-entrypoint.sh"]
 ENTRYPOINT ["/docker-entrypoint.sh"]
 
-CMD ["haproxy", "-f", "/usr/local/etc/haproxy/haproxy.cfg"]
+
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+#haproxy -f /usr/local/etc/haproxy/haproxy.cfg
+#fail2ban-client -b start
 touch  /opt/logdistro/logs/newbans.log
 
 tail -f /var/log/fail2ban.log /opt/logdistro/logs/newbans.log
